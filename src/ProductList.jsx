453bd990c482d46390120a8c845ed31008e0867e
Sarah Hull <sarahthull@gmail.com>
2025-03-19 13:09:44 -0400

    Updating product list config

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -311,21 +311,22 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-                    {plantsArray.map((category, index) =>
-                    <div key={index}>
-                        <h2>{category.category}</h2>
-                        <ul>
-                        {category.plants.map(member => (
-                            <li key={member.index}>
-                                <h3>{member.name}</h3> 
-                                <img src={member.image} alt={member.name} width="100"/> 
-                                <p> cost: {member.cost}</p> 
-                                <p>description: {member.description}</p>
-                            </li>
+                        {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1><div>{category.category}</div></h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) => (
+                                <div className="product-card" key={plantIndex}>
+                                    <img className="product-image" src={plant.image} alt={plant.name} />
+                                    <div className="product-title">{plant.name}</div>
+                                    <div className="product-description">{plant.description}</div>
+                                    <div className="product-cost">{plant.cost}</div>
+                                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                </div>
+                                ))}
+                            </div>
+                        </div>
                         ))}
-                        </ul>
-                    </div>
-                )}
                  
                 </div>
             
