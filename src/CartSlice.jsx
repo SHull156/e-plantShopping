5bb9fafb97beb41632b128ed2aa5e515aece3587
Sarah Hull <sarahthull@gmail.com>
2025-03-20 08:10:39 -0400

    added state managementt

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,17 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const {name, image, cost} = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({name, image, cost, quanitity: 1});
+            }
+        },
     
     },
     removeItem: (state, action) => {
+        state.items = state.items.filer(item => item.name !== action.payload);
+        
     },
     updateQuantity: (state, action) => {
+        const {name, quantity} = action.payload;
+        const itemToUpdate = state.items.find(item => item.name ===name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+        }
 
     
     },
   },
-});
+);
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
